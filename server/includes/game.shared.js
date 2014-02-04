9e922f73601019bb3170b8cf73003a94974e2325
Stefano Balietti <futur.dorko@gmail.com>
2014-02-04 12:49:47 +0100

    mods

--- server/includes/game.shared.js
+++ server/includes/game.shared.js
@@ -12,11 +12,11 @@ module.exports = {
     // Group settings.
 
     // How many players have to connect before a random subset is drawn.
-    POOL_SIZE: 2,
+    POOL_SIZE: 4,
     // How many players in each group ( must be <= POOL_SIZE).
-    GROUP_SIZE: 2,
+    GROUP_SIZE: 4,
     // Minimum number of players that must be always connected.
-    MIN_PLAYERS: 2,
+    MIN_PLAYERS: 4,
 
     // Session Counter start from.
     SESSION_ID: 100,
@@ -31,7 +31,7 @@ module.exports = {
     CHOSEN_TREATMENT: 'pp',
 
     // Number or rounds to repeat the bidding. *
-    REPEAT: 1,
+    REPEAT: 20,
 
     // Number of coins to split. *
     COINS: 100,
@@ -43,7 +43,7 @@ module.exports = {
     DEBUG: true,
 
     // AUTO-PLAY.
-    AUTO: false
+    AUTO: true
 
     // * =  If you change this, you need to update the instructions and quiz
 };
