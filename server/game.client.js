6aedea7c668424a1d5db10e6a313e986b4072331
lmis <lmiserez@student.ethz.ch>
2014-08-07 13:45:25 +0200

    adapted game.client.js to new VisualTimer.js methods; fixed minor issues;

--- server/game.client.js
+++ server/game.client.js
@@ -75,11 +75,9 @@ module.exports = function(gameRoom, treatmentName, settings) {
 
         node.on('BID_DONE', function(offer, to) {
             var root;
-            // TODO: check this timer obj.
-            node.game.timer.switchActiveBoxTo(node.game.timer.waitBox,{waitTime: 30000});
-            node.game.timer.waitBox.unhideBox();
-            node.game.timer.mainBox.setClassNameBody('strike');
-            node.game.timer.updateDisplay();
+
+            delete node.game.timer.options.timeup;
+            node.game.timer.startWaiting({milliseconds: 5000});
             
             W.getElementById('submitOffer').disabled = 'disabled';
             node.set('offer', offer);
@@ -262,8 +260,6 @@ module.exports = function(gameRoom, treatmentName, settings) {
 
     function ultimatum() {
 
-        node.game.timer.stop();
-
         //////////////////////////////////////////////
         // nodeGame hint:
         //
@@ -314,18 +310,14 @@ module.exports = function(gameRoom, treatmentName, settings) {
 
                 // Start the timer after an offer was received.
                 options = {
-                    milliseconds: 30000,
+                    milliseconds: 10000,
                     timeup: function() { 
                         node.emit('BID_DONE', Math.floor(1 +
                                                          Math.random()*100), other);
                     }
                 };
                 
-                node.game.timer.restart(options);
-                node.game.timer.switchActiveBoxTo(node.game.timer.mainBox,-1);
-                node.game.timer.mainBox.unhideBox();
-                node.game.timer.mainBox.setClassNameBody('');
-                node.game.timer.waitBox.hideBox();
+                node.game.timer.startTiming(options);
 
                 b = W.getElementById('submitOffer');
 
@@ -378,13 +370,11 @@ module.exports = function(gameRoom, treatmentName, settings) {
 
             W.loadFrame('/ultimatum/html/resp.html', function() {
                 options = {
-                    milliseconds: 30000
+                    milliseconds: 10000
                 };
                 
-                node.game.timer.restart(options);
-                node.game.timer.switchActiveBoxTo(node.game.timer.waitBox);
+                node.game.timer.startWaiting(options);
                 node.game.timer.mainBox.hideBox();
-                node.game.timer.waitBox.unhideBox();
 
                 //////////////////////////////////////////////
                 // nodeGame hint:
@@ -412,7 +402,7 @@ module.exports = function(gameRoom, treatmentName, settings) {
                             that.randomAccept(msg.data, other);
                         }
                     };
-                    node.game.timer.restart(options);
+                    node.game.timer.startTiming(options);
                     
 
                     offered = W.getElementById('offered');
@@ -460,6 +450,8 @@ module.exports = function(gameRoom, treatmentName, settings) {
 
     function endgame() {
         W.loadFrame('/ultimatum/html/ended.html', function() {
+            node.game.timer.switchActiveBoxTo(node.game.timer.mainBox);
+            node.game.timer.waitBox.hideBox();
             node.game.timer.setToZero();
             node.on.data('WIN', function(msg) {
                 var win, exitcode, codeErr;
@@ -544,7 +536,7 @@ module.exports = function(gameRoom, treatmentName, settings) {
         cb: instructions,
         minPlayers: [ MIN_PLAYERS, notEnoughPlayers ],
         // syncOnLoaded: true,
-        timer: 90000,
+        timer: 9000,
         done: clearFrame
     });
 
@@ -561,7 +553,7 @@ module.exports = function(gameRoom, treatmentName, settings) {
         //  - an object containing properties _milliseconds_, and _timeup_
         //     the latter being the name of the event to fire (default DONE)
         // - or a function returning the number of milliseconds.
-        timer: 60000,
+        timer: 1000,
         done: function() {
             var b, QUIZ, answers, isTimeup;
             QUIZ = W.getFrameWindow().QUIZ;
