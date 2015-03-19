1ddf966dbc403f528d489e21b3a92274eb83035e
Stefano Balietti <futur.dorko@gmail.com>
2015-03-19 12:08:42 +0100

    total bonus consistent with exchange rate coins and rounds

--- views/contexts/de/instructions.js
+++ views/contexts/de/instructions.js
@@ -4,13 +4,15 @@ module.exports = function(settings, headers) {
     var R = settings.standard.REPEAT;
     var E = settings.standard.EXCHANGE_RATE_INSTRUCTIONS;
 
+    var B = (C*R) * (E/C);
+
     return {
         title: "Instruktionen",
         instructions: "Anleitung zum Ultimatum Spiel. Bitte sorgfältig lesen.",
         thisGame: "This game is played in rounds by two human players randomly paired.",
         inEachRound: 'In each round, one of the them, called <em>BIDDER</em>, makes an offer to the other player, called <em>RESPONDENT</em>, about how to share ' + C + ' ECU (Experimental Currency). ' + C + ' ECU are equal to ' + E + ' USD.',
         theRespondent: "The RESPONDENT can either accept or reject the offer of the BIDDER. If he / she accepts, both players split " + C + " ECU accordingly, else both get 0.",
-        theGame: "The game is repeated " + R + " rounds, therefore the maximum bonus available is 1 dollar.",
+        theGame: "The game is repeated " + R + " rounds, therefore the maximum bonus available is " + B + " dollar/s.",
         ifYouUnderstood: "If you understood the instructions correctly press the button to proceed to the game.",
         proceed: "Proceed to the game"
     };
--- views/contexts/de/instructions_pp.js
+++ views/contexts/de/instructions_pp.js
@@ -4,6 +4,8 @@ module.exports = function(settings, headers) {
     var R = settings.pp.REPEAT;
     var E = settings.pp.EXCHANGE_RATE_INSTRUCTIONS;
     var W = settings.pp.WAIT_TIME;
+    
+    var B = (C*R) * (E/C);
 
     return {
         title: "INSTRUKTIONEN",
@@ -11,7 +13,7 @@ module.exports = function(settings, headers) {
         thisGameIsPlayed: "This game is played in rounds by two human players randomly paired.",
         inEachRound: 'In each round, one of the them, called <em>BIDDER</em>, makes an offer to the other player, called <em>RESPONDENT</em>, about how to share ' + C + ' ECU (Experimental Currency). ' + C + ' ECU are equal to ' + E + ' USD.',
         theRespondent: "The RESPONDENT can either accept or reject the offer of the BIDDER. If he / she accepts, both players split " + C + " ECU accordingly, else both get 0.",
-        theGameIsRepeated: "The game is repeated " + R + " rounds.",
+        theGameIsRepeated: "The game is repeated " + R + " rounds, therefore the maximum bonus available is " + B + " dollar/s.",
         important: "Important. If one of the players disconnects for more than " + W + " seconds the game will be terminated.",
         inSuchACase: "In such a case the player who disconnected will not be paid at all, and the remaining ones will be paid only the show up fee.",
         ifYouUnderstood: "If you understood the instructions correctly press the button to proceed to the game.",
