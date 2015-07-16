6e684deab14e3eddce11cda5445fa439cddbed3a
Stefano Balietti <futur.dorko@gmail.com>
2015-07-16 17:11:40 +0200

    commented transport option for testing

--- test/settings.js
+++ test/settings.js
@@ -1,4 +1,5 @@
 module.exports = {
-    sioTransports: ["websocket", "flashsocket", "htmlfile", "xhr-polling", "jsonp-polling"],
+    // Not working in Express 4 at the moment.
+    // sioTransports: ["websocket", "flashsocket", "htmlfile", "xhr-polling", "jsonp-polling"],
     numPlayers: 2
 };
