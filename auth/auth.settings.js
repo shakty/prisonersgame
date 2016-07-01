1d072221d5d790cc70c2af8258764da4e8ef0926
Stefano Balietti <futur.dorko@gmail.com>
2016-07-01 14:11:37 -0400

    mods

--- auth/auth.settings.js
+++ auth/auth.settings.js
@@ -9,7 +9,7 @@
 
 module.exports = {
 
-    enabled: true, // [true, false] Default: TRUE.
+    enabled: false, // [true, false] Default: TRUE.
 
     mode: 'auto', // ['remote', 'local', 'auto'] Default: 'auto'
 
