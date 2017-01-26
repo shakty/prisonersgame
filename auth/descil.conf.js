d2c3896a11ea161250d06621748bff9ffe6d57ba
Stefano Balietti <futur.dorko@gmail.com>
2017-01-26 11:18:38 -0500

    cleanup in auth dir

--- auth/descil.conf.js
+++
@@ -1,4 +0,0 @@
-module.exports.key = "YOUR_KEY_HERE";
-module.exports.project = "YOUR_PROJECT_NAME_HERE";
-module.exports.uri = "YOUR_AUTH_SERVER_HERE";
-module.exports.file = __dirname + '/' + 'auth_codes.js';
